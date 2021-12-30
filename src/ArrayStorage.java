c20085b10292e2cc1719c4b40d0708736943cf49
Margarita <vmargaritka@gmail.com>
2021-12-30 21:47:52 +0700

    HW 1 basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,53 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int index = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        index = 0;
     }
 
     void save(Resume r) {
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                storage[i] = r;
+                return;
+            }
+        }
+        storage[index++] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[--index];
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, index);
     }
 
     int size() {
-        return 0;
+        return index;
     }
 }
